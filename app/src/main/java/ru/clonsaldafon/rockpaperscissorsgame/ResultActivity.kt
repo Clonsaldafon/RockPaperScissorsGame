574fab3f93444ac21e91c76a6a0385ef54010697
Anton Bezmelnitsin <a.bezmelnitsin.a@yandex.ru>
2024-09-21 23:13:23 +0500

    Refactoring

--- app/src/main/java/ru/clonsaldafon/rockpaperscissorsgame/ResultActivity.kt
+++ app/src/main/java/ru/clonsaldafon/rockpaperscissorsgame/ResultActivity.kt
@@ -1,15 +1,27 @@
 package ru.clonsaldafon.rockpaperscissorsgame
 
-import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ResultActivity : AppCompatActivity() {
+    private var rockText: String? = null
+    private var scissorsText: String? = null
+    private var paperText: String? = null
+
+    companion object {
+        const val USER_KEY = "result_user_key"
+        const val COMPUTER_KEY = "result_computer_key"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        rockText = getString(R.string.rock_text)
+        scissorsText = getString(R.string.scissors_text)
+        paperText = getString(R.string.paper_text)
 
         val textUserChoice = findViewById<TextView>(R.id.text_user_choice)
         val textComputerChoice = findViewById<TextView>(R.id.text_computer_choice)
@@ -23,22 +35,14 @@ class ResultActivity : AppCompatActivity() {
         checkWinner(userChoice!!, computerChoice!!)
     }
 
-    companion object {
-        const val USER_KEY = "result_user_key"
-        const val COMPUTER_KEY = "result_computer_key"
-    }
-
     private fun checkWinner(userChoice: String, computerChoice: String) {
         val textWinner = findViewById<TextView>(R.id.text_winner)
 
         when {
             userChoice == computerChoice -> textWinner.text = getString(R.string.draw)
-            (userChoice == getString(R.string.rock_text) &&
-                    computerChoice == getString(R.string.scissors_text)) ||
-                    (userChoice == getString(R.string.scissors_text) &&
-                            computerChoice == getString(R.string.paper_text)) ||
-                    (userChoice == getString(R.string.paper_text) &&
-                            computerChoice == getString(R.string.rock_text)) -> {
+            (userChoice == rockText && computerChoice == scissorsText) ||
+                    (userChoice == scissorsText && computerChoice == paperText) ||
+                    (userChoice == paperText && computerChoice == rockText) -> {
                                 textWinner.text = getString(R.string.you_win)
                                 textWinner.setTextColor(Color.GREEN)
                             }
